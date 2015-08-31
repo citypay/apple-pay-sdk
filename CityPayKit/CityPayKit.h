60186776402212e72c74121f3ec338ce331f7447
Gary Feltham <gary.feltham@citypay.com>
2015-08-31 19:37:29 +0800

    Initial Commit

---
+++ CityPayKit/CityPayKit.h
@@ -0,0 +1,19 @@
+//
+//  CityPayKit.h
+//  CityPayKit
+//
+//  Created by Gary Feltham on 31/08/2015.
+//  Copyright (c) 2015 CityPay Limited. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CityPayKit.
+FOUNDATION_EXPORT double CityPayKitVersionNumber;
+
+//! Project version string for CityPayKit.
+FOUNDATION_EXPORT const unsigned char CityPayKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CityPayKit/PublicHeader.h>
+
+
