656fba93a499a1372da15d558498a6600bb4f304
Roger Bisson <roger.bisson@citypay.com>
2015-10-28 16:45:07 +0000

    Refactored / improved code structure, and internal documentation for CityPayKit.

--- CityPayKit/CityPayResponse.swift
+++ CityPayKit/CityPayResponse.swift
@@ -11,21 +11,33 @@ import CommonCrypto
 
 /**
 
-The CPResponse models the response object from the gateway for generic processing
-using the **CityPay HTTP PayPOST API**.
+ The CityPayResponse class models the response data received from the CityPay
+ payment gateway.
+ 
+ To determine if a transaction has been accepted review the *authorised*
+ parameter and ensure you know if you are in *test* mode or *live* mode.
 
-To determine if a transaction has been accepted review the *authorised* parameter and ensure you know if you are in *test* mode or *live*
-
-Initialisation of a CPResponse instance is provided by the API as a JSON packet which can be reinstated
-using standard JSON serialization methods and
-
-    let jsonObject: AnyObject? = NSJSONSerialization.JSONObjectWithData(data, options:NSJSONReadingOptions.AllowFragments, error:&parseError)
+ The CityPayResponse object is initialized by passing a JSON packet as
+ a parameter to the principal designated initializer. The JSON packet
+ is processed using standard JSON serialization methods:
+ 
+    ``
+    let jsonObject: AnyObject?
+        = NSJSONSerialization
+            .JSONObjectWithData(
+                data,
+                options: NSJSONReadingOptions.AllowFragments,
+                error:&parseError
+            )
 
     if let json = jsonObject as? NSDictionary {
-        let response = CPResponse(json)
+        let response = CityPayResponse(json)
     } else {
-        // error checking code.
+        //
+        //  Error checking code.
+        //
     }
+    ``
 
 */
 public class CityPayResponse: NSObject {
@@ -54,6 +66,9 @@ public class CityPayResponse: NSObject {
     public let postcode: String?
     public let transno: Int
     
+    /**
+
+     */
     private func b64_sha256(data : NSData) -> String {
         var hash = [UInt8](count: Int(CC_SHA256_DIGEST_LENGTH), repeatedValue: 0)
         CC_SHA256(data.bytes, CC_LONG(data.length), &hash)
@@ -61,91 +76,94 @@ public class CityPayResponse: NSObject {
         return res.base64EncodedStringWithOptions(NSDataBase64EncodingOptions())
     }
     
-    func log() -> String {
+    /**
+
+     */
+    private func log() -> String {
         return "RS:\(identifier),amount=\(amount),card=\(maskedPan),\(expMonth)\(expYear),authorised=\(authorised),mode=\(mode)"
     }
     
-    /// Determines if the data provided is valid based on the sha256 value. The licence key provides a salt
-    /// into the hash function
-    public func isValid(licenceKey: String) -> Bool {
-       
-        var str = authcode ?? ""
-        str += String(amount) +
-            errorcode +
-            String(merchantId) +
-            String(transno) +
-            identifier +
-            licenceKey
-        
-        if let data = (str as NSString).dataUsingEncoding(NSUTF8StringEncoding) {
-            let b64 = b64_sha256(data)
-            return b64 == sha256
-        }
-        return false
+    /**
+     
+     A private designated initializer for the CityPayResponse object.
+     
+     - parameters
+     
+     */
+    private init(
+        amount: Int,
+        currency: String,
+        authcode: String?,
+        authorised: Bool,
+        AvsResponse: String?,
+        CscResponse: String?,
+        errorcode: String,
+        errormsg: String,
+        expMonth: Int,
+        expYear: Int,
+        identifier: String,
+        maskedPan: String,
+        merchantId: Int,
+        mode: String,
+        result: Int,
+        sha256: String,
+        status: String,
+        title: String?,
+        firstname:String?,
+        lastname:String?,
+        email: String?,
+        postcode: String?,
+        transno: Int
+    ) {
+        self.amount = amount
+        self.currency = currency
+        self.authcode = authcode
+        self.authorised = authorised
+        self.AvsResponse = AvsResponse
+        self.CscResponse = CscResponse
+        self.errorcode = errorcode
+        self.errormsg = errormsg
+        self.expMonth = expMonth
+        self.expYear = expYear
+        self.identifier = identifier
+        self.maskedPan = maskedPan
+        self.merchantId = merchantId
+        self.mode = mode
+        self.result = result
+        self.sha256 = sha256
+        self.status = status
+        self.title = title
+        self.firstname = firstname
+        self.lastname = lastname
+        self.email = email
+        self.postcode = postcode
+        self.transno = transno
     }
     
-    // Creates a rejected CPResponse from this response. This allows the object cycle to be rejected
-    // by workflow such as an invalid digest information.
-    func rejectAuth(licenceKey: String, errormsg: String) -> CityPayResponse {
-        NSLog("Rejecting auth \(errormsg)")
-        // recreate digest
-        let ec = "099"
-        var str = authcode ?? ""
-        str += String(amount) +
-            ec +
-            String(merchantId) +
-            String(transno) +
-            identifier +
-            licenceKey
-        let data = (str as NSString).dataUsingEncoding(NSUTF8StringEncoding)!
-        let digest = b64_sha256(data)
-        return CityPayResponse(
-            amount:amount, currency:currency, authcode:authcode, authorised:false,
-            AvsResponse:AvsResponse, CscResponse:CscResponse, errorcode:ec, errormsg:errormsg,
-            expMonth:expMonth, expYear:expYear, identifier:identifier, maskedPan:maskedPan,
-            merchantId:merchantId, mode:mode, result:2, sha256:digest,
-            status:status, title:title, firstname:firstname, lastname:lastname,
-            email:email, postcode:postcode, transno:transno)
-    }
+    /**
     
-    init(amount: Int, currency: String, authcode: String?, authorised: Bool,
-        AvsResponse: String?, CscResponse: String?, errorcode: String, errormsg: String,
-        expMonth: Int, expYear: Int, identifier: String, maskedPan: String,
-        merchantId: Int, mode: String, result: Int, sha256: String,
-        status: String, title: String?, firstname:String?, lastname:String?,
-        email: String?,postcode: String?,transno: Int) {
-        self.amount 			    = amount
-        self.currency 			  = currency
-        self.authcode 			  = authcode
-        self.authorised 			= authorised
-        self.AvsResponse 			= AvsResponse
-        self.CscResponse 			= CscResponse
-        self.errorcode 			  = errorcode
-        self.errormsg 			  = errormsg
-        self.expMonth 			  = expMonth
-        self.expYear 			    = expYear
-        self.identifier 			= identifier
-        self.maskedPan 			  = maskedPan
-        self.merchantId 			= merchantId
-        self.mode 			      = mode
-        self.result 			    = result
-        self.sha256 			    = sha256
-        self.status 			    = status
-        self.title 			      = title
-        self.firstname 			  = firstname
-        self.lastname 			  = lastname
-        self.email 			      = email
-        self.postcode 			  = postcode
-        self.transno 			    = transno
-    }
-
-    /// only way to initialise is via a JSON packet
+     A public designated initializer for the CityPayResponse object.
+    
+     - parameters
+    
+       - data:
+         contains the JSON encoded response received from the CityPay
+         payment gateway.
+    
+     */
     public init(data: NSData) {
+        
         var e: NSError?
-        let json = JSON(data: data, options: NSJSONReadingOptions.AllowFragments, error: &e)
+        let json = JSON(
+            data: data,
+            options: NSJSONReadingOptions.AllowFragments,
+            error: &e
+        )
+        
         if let error = e {
             NSLog("Error parsing JSON \(error)")
         }
+        
         self.amount = json["amount"].int ?? 0
         self.currency = json["currency"].stringValue
         self.authcode = json["authcode"].string
@@ -169,12 +187,81 @@ public class CityPayResponse: NSObject {
         self.email = json["email"].string
         self.postcode = json["postcode"].string
         self.transno = json["transno"].int ?? -1
-        
-        
     }
     
+    /**
+
+     Determines if the data provided is valid based on the SHA-256 value.
+     The licence key provides a salt into the hash function.
+     
+     */
+    public func isValid(licenceKey: String) -> Bool {
+       
+        var str = authcode ?? ""
+        str += String(amount) +
+            errorcode +
+            String(merchantId) +
+            String(transno) +
+            identifier +
+            licenceKey
+        
+        if let data = (str as NSString).dataUsingEncoding(NSUTF8StringEncoding) {
+            let b64 = b64_sha256(data)
+            return b64 == sha256
+        }
+        return false
+    }
     
+    /**
     
+     Creates a "rejected transaction" CityPayResponse object based on the
+     content of the instant response. This allows an alternate CityPayResponse
+     to generated programmatically in the event that, for example, invalid
+     digest information is identified.
+
+     */
+    func rejectAuth(licenceKey: String, errormsg: String) -> CityPayResponse {
+        NSLog("Rejecting auth \(errormsg)")
+        
+        //
+        //  Re-calculate CityPayResponse SHA-256 digest value.
+        //
+        let ec = "099"
+        var str = authcode ?? ""
+        str += String(amount) +
+            ec +
+            String(merchantId) +
+            String(transno) +
+            identifier +
+            licenceKey
+        let data = (str as NSString).dataUsingEncoding(NSUTF8StringEncoding)!
+        let digest = b64_sha256(data)
+        return CityPayResponse(
+            amount: self.amount,
+            currency: self.currency,
+            authcode: self.authcode,
+            authorised: false,
+            AvsResponse: self.AvsResponse,
+            CscResponse: self.CscResponse,
+            errorcode: ec,
+            errormsg: errormsg,
+            expMonth: self.expMonth,
+            expYear: self.expYear,
+            identifier: self.identifier,
+            maskedPan: self.maskedPan,
+            merchantId: self.merchantId,
+            mode: self.mode,
+            result: 2,
+            sha256: digest,
+            status: status,
+            title: self.title,
+            firstname: self.firstname,
+            lastname: self.lastname,
+            email: self.email,
+            postcode: self.postcode,
+            transno: self.transno
+        )
+    }
     
 }
 
